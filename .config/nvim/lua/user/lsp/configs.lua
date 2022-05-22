fbd26fc3f780df838c0709625ff92f6676aa839e
ffqi <ffqi@outlook.com>
2022-05-22 12:50:24 +0800

    modify lsp config

--- .config/nvim/lua/user/lsp/configs.lua
+++ .config/nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua" }
+local servers = { "jsonls", "sumneko_lua", "clangd", "ccls","pyright","zk","vuels","eslint","ltex","gopls" }
 
 lsp_installer.setup {
 	ensure_installed = servers
