433ef072d7fae7e8348deaf0c4cc86fbc51c32e7
ffqi <ffqi@outlook.com>
2022-06-30 22:57:46 +0800

    update lsp config

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -15,6 +15,8 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
     formatting.clang_format,
-    -- diagnostics.flake8
+    -- diagnostics.flake8,
+    formatting.fixjson,
+    formatting.shfmt,
 	},
 })
